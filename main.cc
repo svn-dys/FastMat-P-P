5f99c52fb1d86513fd9e5417ec1508df31c38f25
svn-dys <xpsvendysthe@gmail.com>
2024-11-13 20:42:52 -0500

    Rewrite code that is not standard to C++ conventions. Finish implementation of matrix multiplication.

--- main.cc
+++ main.cc
@@ -21,18 +21,20 @@ public:
     
     // Copy Assignment Operator
     Matrix2D& operator=(const Matrix2D& rhs) noexcept {
-        if (this != &rhs) {
-            delete[] this->matrix_arr_;
+        if (this != &rhs) 
+        {
+            delete[] matrix_arr_;
             
-            this->rows_ = rhs.rows_;
-            this->columns_ = rhs.columns_;
-            this->matrix_arr_ = new size_t[rows_ * columns_];
+            rows_ = rhs.rows_;
+            columns_ = rhs.columns_;
+            matrix_arr_ = new size_t[rows_ * columns_];
 
             for(size_t i = 0; i < rows_ * columns_; i++) 
             {
-                this->matrix_arr_[i] = rhs.matrix_arr_[i]; // TODO: optimize 
+                matrix_arr_[i] = rhs.matrix_arr_[i]; // TODO: optimize 
             }
         }
+
         return *this;
     }
 
@@ -45,18 +47,23 @@ public:
     }
 
     // Matrix multiplication.
-    void operator*(Matrix2D& rhs) {
-        if (this->GetColumnsSize() != rhs.GetRowsSize())
+    Matrix2D operator*(Matrix2D& rhs) {
+        if (columns_ != rhs.rows_)
             throw std::invalid_argument("Left hand matrix's columns length must be equal to right hand matrix's row/s length.");
         
-        Matrix2D product_matrix(this->GetRowsSize(), rhs.GetColumnsSize());
-        for (size_t i = 0; i < this->GetRowsSize(); i++) {
-            for (size_t j = 0; j < rhs.GetColumnsSize(); j++) {
-                product_matrix(i,j) = this->operator()(i,j) * rhs(i, j);
+        Matrix2D product_matrix(rows_, rhs.columns_);
+        for (size_t row = 0; row < rows_; row++) 
+        {
+            for (size_t col = 0; col < rhs.columns_; col++) 
+            {
+                for (size_t k = 0; k < columns_; k++)
+                {
+                    product_matrix(row, col) += (*this)(row, k) * rhs(k, col);
+                }
             }
         }
 
-        product_matrix.PrintMatrix();
+        return product_matrix;
     }
 
     // Getters & Setters
@@ -68,11 +75,14 @@ public:
     void PrintMatrix() {
         LOG("Matrix:")
         LOG("------------------------------------------------------------------");
-        for (size_t i = 0; i < GetRowsSize(); ++i) {
+        for (size_t i = 0; i < rows_; ++i) 
+        {
             std::cout << "{ ";
-            for (size_t j = 0; j < GetColumnsSize(); ++j) {
+            for (size_t j = 0; j < columns_; ++j) 
+            {
                 std::cout << this->operator()(i,j);
-                if (j < columns_ - 1) {
+                if (j < columns_ - 1) 
+                {
                     std::cout << ", ";
                 }
             }
@@ -88,14 +98,21 @@ private:
 
 
 int main()
-{       
-    Matrix2D m_B(2,3);
+{           
+    Matrix2D m_B(3,3);
     m_B(0,0) = 2; m_B(0,1) = 3; m_B(0,2) = 1; 
     m_B(1,0) = 3; m_B(1,1) = 5; m_B(1,2) = 6; 
-    Matrix2D m_C(3,2);
-    m_C(0,0) = 2; m_C(0,1) = 3; 
-    m_C(1,0) = 3; m_C(1,1) = 5;
+    m_B(2,0) = 3; m_B(2,1) = 5; m_B(2,2) = 6; 
+    Matrix2D m_C(3,3);
+    m_C(0,0) = 2; m_C(0,1) = 3; m_C(0, 2) = 4;
+    m_C(1,0) = 3; m_C(1,1) = 5; m_C(1,2) = 5;
+    m_C(2,0) = 3; m_C(2,1) = 3; m_C(2,2) = 5;
+    m_B.PrintMatrix();
+    m_C.PrintMatrix();
 
-    m_B * m_C;
+    Matrix2D m_product(0,0);
+    m_product = m_B * m_C;
+    m_product.PrintMatrix();
+
     return 0;
 }
